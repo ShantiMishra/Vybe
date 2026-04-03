1be7fbe585571f606bf0735733ea54622ecaf963
ShantiMishra <shantimishra2006@gmail.com>
2026-04-03 09:04:55 +0530

    User model created

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -26,6 +26,16 @@ const userSchema = new mongoose.Schema({
         {type:mongoose.Schema.Types.ObjectId,   
             ref:"User"
         }
+    ],
+    following:[
+        {type:mongoose.Schema.Types.ObjectId,   
+            ref:"User"
+        }
     ]
-
+    
 },{timestamps:true})
+
+
+const User = mongoose.model("User",userSchema)
+
+export default User
