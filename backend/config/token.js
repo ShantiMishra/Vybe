28b06f98f7991163ab3b6f86db10da1c99d79588
ShantiMishra <shantimishra2006@gmail.com>
2026-04-03 21:06:20 +0530

    created Sign In controller

--- backend/config/token.js
+++ backend/config/token.js
@@ -2,8 +2,12 @@ import jwt from "jsonwebtoken"
 
 const genToken=async (userId)=>{
   try {
-    const token = await jwt.sign({userId},)
+    const token = await jwt.sign({userId},process.env.JWT_SCERET,{expiresIn:"10y"})
+    return token;
+
   } catch (error) {
-    
+    return res.status(500).json(`gen token error ${error}`)
   }
 }
+
+export default genToken;
--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,3 +1,4 @@
+import genToken from "../config/token.js"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
@@ -12,9 +13,13 @@ export const signUp = async (req,res)=>{
         }
         
         // Find by username 
-        const findByUserName = await User.findOne({email})
+        const findByUserName = await User.findOne({userName})
         if(findByUserName){
             return res.status(400).json({message:"UserName already exist !"})
+        }
+
+        if(password.length<6){
+            
         }
 
         const hassedpassword = await bcrypt.hash(password,10)
@@ -26,9 +31,58 @@ export const signUp = async (req,res)=>{
             password:hassedpassword
         })
 
+        const token = await genToken(user._id)
+
+        res.cookie("token",token,{
+            httpOnly:true,
+            maxAge:10*365*24*60*60*1000,
+            secure:false,
+            sameSite:"Strict"
+        })
+
+        return res.status(201).json(user)
+
         
 
     } catch (error) {
+        return res.status(500).json({message:`signup error ${error}`})
+    }
+}
+
+
+export const signIn = async (req,res)=>{
+    try {
+        const {password,userName} = req.body
+         
+       
+        // Find by username 
+        const user = await User.findOne({userName})
+        if(!user){
+            return res.status(400).json({message:"User Not Found !"})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            return res.status(400).json({message:"Incorrect Password !"})
+        }
+
+       
+
+        const token = await genToken(user._id)
+
+        res.cookie("token",token,{
+            httpOnly:true,
+            maxAge:10*365*24*60*60*1000,
+            secure:false,
+            sameSite:"Strict"
+        })
+
+        return res.status(201).json(user)
+
         
+
+    } catch (error) {
+        return res.status(500).json({message:`signup error ${error}`})
     }
 }
