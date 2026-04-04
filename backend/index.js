c72caf7d6ddd1c8aab58a33ac65d955eaa4af7c4
ShantiMishra <shantimishra2006@gmail.com>
2026-04-04 08:28:55 +0530

    added signup,signin and signout controllers

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./config/db.js"
+import cookieParser from "cookie-parser"
+import cors from 'cors'
+import authRouter from "./routes/auth.routes.js"
 dotenv.config()
 
 const app = express()
 
 const port = process.env.PORT 
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    withCredentials:true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth",authRouter)  
+
+
 
 app.listen(port,()=>{
     connectDb()
---
+++ backend/middlewares/isAuth.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken'
+
+const isAuth = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
